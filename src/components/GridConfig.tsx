68b94be44844fa01570afbea4b96444325e8641a
foobar <uagnl25x@anonaddy.me>
2024-02-01 23:39:55 +0530

    Added more options from zustand store

--- src/components/GridConfig.tsx
+++ src/components/GridConfig.tsx
@@ -1,11 +1,11 @@
-import { Button, ButtonGroup, MenuItem, Paper, TextField } from '@mui/material'
+import { Box, Button, ButtonGroup, MenuItem, Paper, Slider, TextField } from '@mui/material'
 import { memo, useRef, useState } from 'react'
-import { Grid, SearchAlgorithm, searchAlgorithms } from '../types'
-import { constructGrid } from '../App'
+import { Grid, GridConfigActions, SearchAlgorithm, searchAlgorithms } from '../types'
 import { dfs } from '../algorithms/dfs'
 import { astar } from '../algorithms/astar'
 import { dijkstra } from '../algorithms/dijkstra'
 import { bfs } from '../algorithms/bfs'
+import { constructGrid, useGridConfig } from '../stateStore/gridConfigStore'
 
 export const solveGrid = (grid: Grid, algorithm: SearchAlgorithm): number[] => {
     switch (algorithm) {
@@ -24,7 +24,7 @@ export const solveGrid = (grid: Grid, algorithm: SearchAlgorithm): number[] => {
 
 type GridConfigProps = {
     grid: Grid
-    setGrid: React.Dispatch<React.SetStateAction<Grid>>
+    setGrid: GridConfigActions['setGrid']
     rows: number
     columns: number
 }
@@ -32,12 +32,15 @@ type GridConfigProps = {
 const GridConfig = (props: GridConfigProps) => {
     const { grid, setGrid, rows, columns } = props
 
+    const { animationSpeed, cellSize, density } = useGridConfig()
+    const { changeColumns, changeDensity, changeCellSize, changeAnimationSpeed, changeRows } = useGridConfig.getState()
+
     const [selectedSearchAlgorithm, setSelectedSearchAlgorithm] = useState<SearchAlgorithm>(searchAlgorithms[0])
 
     const timeoutQueue = useRef<ReturnType<typeof setTimeout>[]>([])
 
     const resetGrid = () => {
-        setGrid(constructGrid(rows, columns))
+        setGrid(() => constructGrid(rows, columns))
         dismissOngoingAnimation()
     }
 
@@ -46,50 +49,116 @@ const GridConfig = (props: GridConfigProps) => {
         timeoutQueue.current = []
     }
 
-    const animatePathWithDelay = (cellIndexArr: number[], delayInMs = 50): void => {
+    const animatePathWithDelay = (cellIndexArr: number[], delayInMs = 1000 / animationSpeed): void => {
         let timeout = 0
-        cellIndexArr.forEach(cellIndex => {
-            const timeoutId = setTimeout(
-                () => {
-                    setGrid(p => {
-                        return p.map(cell => {
-                            if (cell.index === cellIndex) return { ...cell, visitedStatus: 'visited' }
-                            return cell
+        const animationSequence = cellIndexArr.map(cellIndex => {
+            return new Promise(res => {
+                const timeoutId = setTimeout(
+                    () => {
+                        setGrid(p => {
+                            return p.map(cell => {
+                                if (cell.index === cellIndex) return { ...cell, visitedStatus: 'visited' }
+                                return cell
+                            })
                         })
+                        res(cellIndex)
+                    },
+                    (timeout += delayInMs),
+                )
+                timeoutQueue.current.push(timeoutId)
+            })
+        })
+        Promise.all(animationSequence).then(cellIndexArr => {
+            // Reset all visited paths to unvisited. This ready ups the grid for another run
+            cellIndexArr.forEach(cellIndex => {
+                setGrid(p => {
+                    return p.map(cell => {
+                        if (cell.index === cellIndex) return { ...cell, visitedStatus: 'unvisited' }
+                        return cell
                     })
-                },
-                (timeout += delayInMs),
-            )
-            timeoutQueue.current.push(timeoutId)
+                })
+            })
         })
     }
 
+    const successAnimation = () => {}
+
     return (
-        <Paper sx={{ display: 'grid', p: 2, justifyContent: 'center' }}>
-            <TextField
-                select
-                value={selectedSearchAlgorithm}
-                label={'search algorithm'}
-                onChange={e => setSelectedSearchAlgorithm(e.target.value as SearchAlgorithm)}
-            >
-                {searchAlgorithms.map(name => (
-                    <MenuItem key={name} value={name}>
-                        {name}
-                    </MenuItem>
-                ))}
-            </TextField>
-            <ButtonGroup sx={{ background: 'white', px: 3, py: 2 }} variant='outlined'>
-                <Button onClick={resetGrid}>Reset</Button>
-                <Button
-                    onClick={() => {
-                        const result = solveGrid(structuredClone(grid), selectedSearchAlgorithm)
-                        animatePathWithDelay(result)
-                    }}
+        <Box
+            sx={{
+                display: 'flex',
+                width: '100%',
+                justifyContent: 'center',
+                alignItems: 'center',
+                gap: 4,
+                flexWrap: 'wrap',
+                p: 4,
+            }}
+        >
+            <Paper sx={{ p: 2 }}>
+                <TextField
+                    select
+                    value={selectedSearchAlgorithm}
+                    label={'search algorithm'}
+                    onChange={e => setSelectedSearchAlgorithm(e.target.value as SearchAlgorithm)}
                 >
-                    solve
-                </Button>
-            </ButtonGroup>
-        </Paper>
+                    {searchAlgorithms.map(name => (
+                        <MenuItem key={name} value={name}>
+                            {name}
+                        </MenuItem>
+                    ))}
+                </TextField>
+            </Paper>
+            <Paper>
+                <ButtonGroup sx={{ background: 'white', px: 3, py: 2 }} variant='outlined'>
+                    <Button onClick={resetGrid}>Reset</Button>
+                    <Button
+                        onClick={() => {
+                            const result = solveGrid(structuredClone(grid), selectedSearchAlgorithm)
+                            animatePathWithDelay(result)
+                        }}
+                    >
+                        solve
+                    </Button>
+                </ButtonGroup>
+            </Paper>
+            <Paper sx={{ p: 2, width: '20%' }}>
+                Rows: {rows}
+                <Slider
+                    step={5}
+                    value={rows}
+                    marks
+                    valueLabelDisplay='auto'
+                    min={10}
+                    max={50}
+                    onChange={(_, v) => (Array.isArray(v) ? changeRows(v[0]) : changeRows(v))}
+                />
+                Columns: {columns}
+                <Slider
+                    value={columns}
+                    step={5}
+                    valueLabelDisplay='auto'
+                    marks
+                    min={10}
+                    max={50}
+                    onChange={(_, v) => (Array.isArray(v) ? changeColumns(v[0]) : changeColumns(v))}
+                />
+            </Paper>
+            <Paper sx={{ p: 2, width: '20%' }}>
+                animation speed
+                <TextField
+                    type='number'
+                    InputProps={{ endAdornment: 'X' }}
+                    size='small'
+                    value={animationSpeed}
+                    onChange={e => {
+                        const value = Number(e.target.value)
+                        if (typeof value === 'number') changeAnimationSpeed(value)
+                    }}
+                    helperText='Greater value yields faster animation'
+                />
+            </Paper>
+        </Box>
     )
 }
 
