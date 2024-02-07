9f942c5a68935df2bb4bbf8b722aa04dcf117627
foobar <uagnl25x@anonaddy.me>
2024-02-07 19:21:25 +0530

    updated zustand store values
    Added option to get post updated version of grid
    removed density
    added extra optional arguments to grid constructor

--- src/stateStore/gridConfigStore.ts
+++ src/stateStore/gridConfigStore.ts
@@ -1,57 +1,59 @@
-import { Grid, GridConfig, GridConfigActions } from '../types'
+import { Grid, GridConfig, GridConfigActions, gridStatus } from '../types'
 import { create } from 'zustand'
 
-export const constructGrid = (rows: number, columns: number): Grid => {
+export const constructGrid = (rows: number, columns: number, startingRow = 0, startingColumn = 0): Grid => {
     if (rows <= 0 || columns <= 0) throw new Error('incorrect value for rows or columns while constructing grid')
 
-    const grid: Grid = []
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < columns; j++) {
-            grid.push({
+    const grid = Array(rows * columns)
+
+    for (let i = startingRow; i < rows; i++) {
+        for (let j = startingColumn; j < columns; j++) {
+            grid[i * columns + j] = {
                 index: i * columns + j,
                 coordinates: [i, j],
                 type: 'open',
                 visitedStatus: 'unvisited',
-            })
+            }
         }
     }
 
-    const getRandom = () => Math.floor(Math.random() * (rows * columns))
+    const randomInt = () => Math.floor(Math.random() * rows * columns)
+    grid[randomInt()].type = 'start'
+    grid[randomInt()].type = 'finish'
 
-    let random = getRandom()
-    grid[random] = { ...grid[random], type: 'start' }
-    random = getRandom()
-    grid[random] = { ...grid[random], type: 'finish' }
-
-    return grid
+    return grid satisfies Grid
 }
 
-export const useGridConfig = create<GridConfig & GridConfigActions>()(set => {
+export const useGridConfig = create<GridConfig & GridConfigActions>()((set, get) => {
     return {
         grid: constructGrid(20, 20),
         setGrid: fn => set(state => ({ grid: fn(state.grid) })),
 
+        getGrid: () => get().grid,
+
+        status: gridStatus['waiting to start'],
+        setGridStatus: status => set(_ => ({ status })),
+
         rows: 20,
         changeRows: (newVal: number) =>
             set(state => {
-                const grid = constructGrid(newVal, state.columns)
-                return { rows: newVal, grid }
+                return { rows: newVal, grid: constructGrid(newVal, state.columns) }
             }),
 
         columns: 20,
         changeColumns: (newVal: number) =>
             set(state => {
-                const grid = constructGrid(state.rows, newVal)
-                return { columns: newVal, grid }
+                return { columns: newVal, grid: constructGrid(state.rows, newVal) }
             }),
 
         cellSize: 20,
-        changeCellSize: (newVal: number) => set(state => ({ cellSize: newVal })),
-
-        density: 3,
-        changeDensity: (newVal: number) => set(state => ({ density: newVal })),
+        changeCellSize: (newVal: number) => set(_ => ({ cellSize: newVal })),
 
         animationSpeed: 30,
-        changeAnimationSpeed: (newVal: number) => set(state => ({ animationSpeed: newVal })),
+        changeAnimationSpeed: (newVal: number) => set(_ => ({ animationSpeed: newVal })),
+
+        actionOnDrag: 'add wall',
+        changeActionOnDrag: () =>
+            set(prev => ({ actionOnDrag: prev.actionOnDrag === 'add wall' ? 'remove wall' : 'add wall' })),
     }
 })
