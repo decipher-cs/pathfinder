147396845fe9f91173bdd13b48d83aa5c60c011a
foobar <uagnl25x@anonaddy.me>
2024-02-12 21:18:27 +0530

    Added new options with solve grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Box, IconButton, Slide } from '@mui/material'
+import { Box, Button, IconButton, Slide } from '@mui/material'
 import './App.css'
 import Grid from './components/Grid'
 import GridConfig from './components/GridConfig'
 import { useState } from 'react'
-import { DragHandle } from '@mui/icons-material'
+import { ArrowLeft, ArrowRight, Settings } from '@mui/icons-material'
 import { FabGroup } from './components/FabGroup'
 
 function App() {
@@ -14,24 +14,32 @@ function App() {
             sx={{
                 height: '100svh',
                 width: '100vw',
-                display: 'grid',
-                gridAutoFlow: 'column',
+                maxWidth: '100vw',
+                display: 'flex',
+                gap: areSettingsVisible ? 5 : 0,
                 overflow: 'visible',
                 position: 'relative',
-                gridTemplateColumns: 'max(0px, 30%) 1fr',
             }}
             className='lattice-bg'
         >
-            <Slide in={areSettingsVisible} direction='right' unmountOnExit mountOnEnter>
-                <Box>
-                    <GridConfig />
-                </Box>
-            </Slide>
-            <Box sx={{ position: 'absolute', right: 0, top: 0, background: 'white' }}>
-                <IconButton onClick={() => setAreSettingsVisible(p => !p)}>
-                    <DragHandle />
-                </IconButton>
+            <Box
+                sx={{
+                    flexBasis: areSettingsVisible ? '30%' : '0px',
+                    overflow: 'hidden',
+                    transition: 'flex 0.3s ease-out',
+                }}
+            >
+                <GridConfig />
             </Box>
+            <Button
+                sx={{ position: 'absolute', borderRadius: '0px', zIndex: areSettingsVisible ? 0 : 999 }}
+                onClick={() => setAreSettingsVisible(p => !p)}
+                variant='contained'
+                size='large'
+                endIcon={areSettingsVisible ? <ArrowLeft /> : <ArrowRight />}
+            >
+                <Settings />
+            </Button>
             <Grid />
             <FabGroup />
         </Box>
