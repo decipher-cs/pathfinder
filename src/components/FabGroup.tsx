ca1c14e953427adcb992bccd0f9bb7b31d13f495
foobar <uagnl25x@anonaddy.me>
2024-03-01 17:57:21 +0530

    Added new FAB to reset grid

--- src/components/FabGroup.tsx
+++ src/components/FabGroup.tsx
@@ -1,26 +1,28 @@
-import { GitHub } from '@mui/icons-material'
+import { GitHub, RestartAltRounded } from '@mui/icons-material'
 import { PlayArrow } from '@mui/icons-material'
 import { Box, Fab } from '@mui/material'
 import { solveGrid } from '../App'
 import { useGridConfig } from '../stateStore/gridConfigStore'
 import { AnimationPlaybackControls } from 'framer-motion'
+import { useShallow } from 'zustand/react/shallow'
 
 type FabGroupProps = { animateCellWithStagger: (arr: number[]) => AnimationPlaybackControls[] }
 
 export const FabGroup = (props: FabGroupProps) => {
     const { animateCellWithStagger } = props
 
-    const { setGrid, getGrid, animationSpeed, selectedAlgorithm } = useGridConfig(s => ({
-        setGrid: s.setGrid,
-        getGrid: s.getGrid,
-        animationSpeed: s.animationSpeed,
-        selectedAlgorithm: s.selectedAlgorithm,
-    }))
+    const { setGrid, selectedAlgorithm, grid } = useGridConfig(
+        useShallow(s => ({
+            grid: s.grid,
+            setGrid: s.setGrid,
+            selectedAlgorithm: s.selectedAlgorithm,
+        })),
+    )
 
     return (
         <Box
             sx={{
-                '& > :not(style)': { m: 1, mb: 2 },
+                '& > :not(style)': { ml: 1 },
                 position: 'fixed',
                 bottom: 0,
                 right: 0,
@@ -38,10 +40,24 @@ export const FabGroup = (props: FabGroupProps) => {
             </Fab>
 
             <Fab
+                onClick={() => {
+                    setGrid(p =>
+                        p.map(c => {
+                            const cell = { ...c }
+                            if (c.visitedStatus === 'visited') cell.visitedStatus = 'unvisited'
+                            if (c.type === 'close') cell.type = 'open'
+                            return Object.is(cell, c) ? c : cell
+                        }),
+                    )
+                }}
+            >
+                <RestartAltRounded sx={{ mr: 0 }} />
+            </Fab>
+
+            <Fab
                 variant='extended'
                 onClick={async () => {
                     try {
-                        const grid = getGrid()
                         const { pathTaken, shortestPath } = await solveGrid(grid, selectedAlgorithm)
 
                         const startingCell = pathTaken.shift()
