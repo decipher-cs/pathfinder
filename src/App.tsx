7afd188b736ec8ae6bf33b0cf6b15b538f69c7fa
foobar <uagnl25x@anonaddy.me>
2024-03-03 17:20:26 +0530

    passed column to worker

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Box, Button } from '@mui/material'
 import './App.css'
 import Grid from './components/Grid'
 import GridConfig from './components/GridConfig'
-import { useState } from 'react'
+import {  useRef, useState } from 'react'
 import { Settings } from '@mui/icons-material'
 import { FabGroup } from './components/FabGroup'
 import { AnimationPlaybackControls, motion, useAnimate } from 'framer-motion'
@@ -11,9 +11,13 @@ import { useGridConfig } from './stateStore/gridConfigStore'
 
 export const worker = new Worker(new URL('./WebWorkers/gridSolver.ts', import.meta.url), { type: 'module' })
 
-export const solveGrid = async (grid: TGrid, algorithm: SearchAlgorithm): Promise<AlgorithmReturnType> => {
+export const solveGrid = async (
+    grid: TGrid,
+    columns: number,
+    algorithm: SearchAlgorithm,
+): Promise<AlgorithmReturnType> => {
     return new Promise((res, rej) => {
-        worker.postMessage({ grid, algorithm } satisfies { grid: TGrid; algorithm: SearchAlgorithm })
+        worker.postMessage({ grid, algorithm, columns })
 
         const handleResult = (e: MessageEvent<AlgorithmReturnType>) => {
             res(e.data)
@@ -32,19 +36,43 @@ export const solveGrid = async (grid: TGrid, algorithm: SearchAlgorithm): Promis
 function App() {
     const [areSettingsVisible, setAreSettingsVisible] = useState(true)
 
-    const animationSpeed = useGridConfig(s => s.animationSpeed)
+    const { animationSpeed } = useGridConfig(s => ({ animationSpeed: s.animationSpeed }))
 
     const [scope, animate] = useAnimate<HTMLDivElement>()
 
-    const animateCellWithStagger = (arr: number[]): AnimationPlaybackControls[] => {
+    const ongoingAnimationControllers = useRef<AnimationPlaybackControls[]>([])
+
+    const cancelAnimations = () => {
+        ongoingAnimationControllers.current.forEach(controller => controller.cancel())
+        ongoingAnimationControllers.current = []
+    }
+
+    const animateCellWithStagger = (arr: number[], postAnimation: () => void): AnimationPlaybackControls[] => {
         if (!scope.current) return []
+        if (ongoingAnimationControllers.current.length > 0) cancelAnimations()
 
         let delay = 0
-        return arr.map(v => {
-            const controller = animate('div.cellIndex' + v, { scale: [null, 0.3, 1, null] }, { delay })
+        const controllers = arr.map((v, i) => {
+            const controller = animate(
+                'div.cellIndex' + v,
+                { scale: [null, 0.3, 1, null] },
+                {
+                    delay,
+                    onComplete() {
+                        if (i === arr.length - 1) {
+                            postAnimation()
+                            ongoingAnimationControllers.current = []
+                        }
+                    },
+                },
+            )
             delay += animationSpeed / 1000
             return controller
         })
+
+        ongoingAnimationControllers.current.push(...controllers)
+
+        return controllers
     }
 
     return (
@@ -66,7 +94,7 @@ function App() {
             </Box>
 
             <Grid ref={scope} />
-            <FabGroup animateCellWithStagger={animateCellWithStagger} />
+            <FabGroup animateCellWithStagger={animateCellWithStagger} cancelAnimations={cancelAnimations} />
             <Button
                 sx={{
                     position: 'absolute',
