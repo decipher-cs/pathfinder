81c92568291844ee0d2f1158a2dbad000b5fb506
foobar <uagnl25x@anonaddy.me>
2024-03-03 17:21:06 +0530

    forwarded ref to grid

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -5,7 +5,7 @@ import { forwardRef, memo, useRef } from 'react'
 import { useShallow } from 'zustand/react/shallow'
 import { CellType } from '../types'
 
-const Grid = forwardRef((props, ref) => {
+const Grid = forwardRef((_, ref) => {
     const { columns, rows } = useGridConfig(useShallow(state => ({ rows: state.rows, columns: state.columns })))
     const arr = Array.from({ length: rows * columns }, (_, i) => i)
 
@@ -13,33 +13,43 @@ const Grid = forwardRef((props, ref) => {
 
     return (
         <Box
-            ref={ref}
             sx={{
                 borderImage: 'linear-gradient(45deg, red, blue)',
                 borderImageSlice: 1,
                 borderStyle: 'solid',
-                borderWidth: '3px',
-
-                justifyContent: 'center',
-                justifyItems: 'center',
-                alignItems: 'center',
-                alignContent: 'center',
+                borderWidth: '8px',
 
                 display: 'grid',
-                gridTemplateRows: `repeat(${rows}, auto)`,
-                gridTemplateColumns: `repeat(${columns}, auto)`,
-                overflow: 'auto',
-
-                gap: 0.3,
-                p: 3,
-                minWidth: '10%',
+                placeContent: 'center',
+                maxWidth: '100%',
                 maxHeight: '100%',
+
+                overflow: 'hidden',
+                p: 3,
             }}
-            // className='custom-scrollbar'
         >
-            {arr.map(i => (
-                <Cell key={i} index={i} cellTypeToPlaceOnClick={cellTypeToPlaceOnClick} />
-            ))}
+            <Box
+                ref={ref}
+                sx={{
+                    display: 'grid',
+
+                    p: 1,
+                    gap: 0.3,
+
+                    gridTemplateRows: `repeat(${rows}, min-content)`,
+                    gridTemplateColumns: `repeat(${columns}, min-content)`,
+
+                    maxWidth: '100%',
+                    maxHeight: '100%',
+                    width: 'fit-content',
+                    height: 'fit-content',
+                    overflow: 'auto',
+                }}
+            >
+                {arr.map(i => (
+                    <Cell key={i} index={i} cellTypeToPlaceOnClick={cellTypeToPlaceOnClick} />
+                ))}
+            </Box>
         </Box>
     )
 })
