0822b80b5d49feb85ae9e74ebd8eafcfe07ffb4c
foobar <uagnl25x@anonaddy.me>
2025-07-27 10:22:26 +0530

    package: add and configure why-did-you-render to optimize app

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react-swc"
 
 // https://vite.dev/config/
-export default defineConfig({
-   plugins: [react(), tailwindcss()],
-})
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react({
+      jsxImportSource: mode === "development" ? "@welldone-software/why-did-you-render" : "react",
+    }),
+    tailwindcss(),
+  ],
+}))
