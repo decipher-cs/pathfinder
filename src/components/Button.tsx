bd662009a17c32cf8eaab38a703c069b419423c7
foobar <uagnl25x@anonaddy.me>
2025-08-02 10:43:07 +0530

    fix: overwrite className with props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,8 +6,8 @@ export default function Button(props: PropsWithChildren<ComponentProps<"button">
   return (
     <button
       className={twMerge(
-        className,
-        "w-fit cursor-pointer rounded-md bg-neutral-800 px-4 py-2 font-semibold text-neutral-200 capitalize transition-[color_background] duration-150 hover:bg-neutral-200 hover:text-neutral-800"
+        "w-fit cursor-pointer rounded-md bg-neutral-800 px-4 py-2 font-semibold text-neutral-200 capitalize transition-[color_background] duration-150 hover:bg-neutral-200 hover:text-neutral-800",
+        className
       )}
       {...btnProps}
     >
