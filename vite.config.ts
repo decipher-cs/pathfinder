9aa5ad5a27a010bf71489c8f735aae88da8105e5
foobar <uagnl25x@anonaddy.me>
2025-08-11 22:13:00 +0530

    add package to analysis app size

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react-swc"
+import { analyzer } from "vite-bundle-analyzer"
+import Terminal from "vite-plugin-terminal"
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -9,5 +11,7 @@ export default defineConfig(({ mode }) => ({
       jsxImportSource: mode === "development" ? "@welldone-software/why-did-you-render" : "react",
     }),
     tailwindcss(),
+    analyzer(),
+    Terminal(),
   ],
 }))
