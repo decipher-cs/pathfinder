5b936936264acaafd993d8e372046bdde8f40c46
foobar <uagnl25x@anonaddy.me>
2025-08-11 22:35:14 +0530

    refactor: improve perf by separating setting body and handle

--- src/components/DraggableSettings.tsx
+++ src/components/DraggableSettings.tsx
@@ -1,4 +1,13 @@
-import { useRef, useState, type ComponentProps, type MouseEventHandler } from "react"
+import {
+  memo,
+  useCallback,
+  useRef,
+  useState,
+  type ComponentProps,
+  type Dispatch,
+  type MouseEventHandler,
+  type SetStateAction,
+} from "react"
 import { useSnapshot } from "valtio"
 import {
   availableAlgorithms,
@@ -14,14 +23,55 @@ import Button from "./Button"
 import { modalProxy } from "../stores/modalStore"
 import { GithubIcon, HistoryIcon } from "lucide-react"
 
-export const DraggableSettings = () => {
+export const DraggableSettings = memo(() => {
   const [position, setPosition] = useState({ x: 3, y: 3 })
 
-  // Store properties of the element used for dragging and the parent element that is being dragged.
-  // settingsPanel is the element being dragged. Height/width are stored to make sure element cannot
-  // be dragged beyond the inner dimensions of the viewport.
-  // Offset is the distance between the settings panel and the coordinates of the cursor inside the said
-  // panel when the first mouse down event fires.
+  const [isCollapsed, setCollapsed] = useState(false)
+
+  const handleCollapse = useCallback(() => setCollapsed((p) => !p), [])
+
+  return (
+    <div
+      className={"absolute z-50 flex max-h-[80svh] w-[40ch] flex-col overflow-hidden rounded-b-md"}
+      style={{
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        resize: isCollapsed ? "none" : "both",
+        height: isCollapsed ? "min-content" : "auto",
+      }}
+    >
+      <SettingHandle
+        isBodyCollapsed={isCollapsed}
+        toggleBodyCollapsed={handleCollapse}
+        setPosition={setPosition}
+      />
+
+      {/* Collapsing Body */}
+      <SettingBody isBodyCollapsed={isCollapsed} />
+    </div>
+  )
+})
+
+type HandleProps = {
+  isBodyCollapsed: boolean
+  toggleBodyCollapsed: () => void
+  setPosition: Dispatch<
+    SetStateAction<{
+      x: number
+      y: number
+    }>
+  >
+}
+
+/* Handle and title */
+const SettingHandle = memo((props: HandleProps) => {
+  /*
+   * Store properties of the element used for dragging and the parent element that is being dragged.
+   * settingsPanel is the element being dragged. Height/width are stored to make sure element cannot
+   * be dragged beyond the inner dimensions of the viewport.
+   * Offset is the distance between the settings panel and the coordinates of the cursor inside the said
+   * panel when the first mouse down event fires.
+   */
+
   const dragStateRef = useRef({
     offsetX: 0,
     offsetY: 0,
@@ -57,7 +107,7 @@ export const DraggableSettings = () => {
     const screenEdgeX = window.innerWidth
     const screenEdgeY = window.innerHeight
 
-    setPosition((prev) => ({
+    props.setPosition((prev) => ({
       x: dragStateRef.current.settingPanelWidth + x > screenEdgeX || x < 0 ? prev.x : x,
       y: dragStateRef.current.settingPanelHeight + y > screenEdgeY || y < 0 ? prev.y : y,
     }))
@@ -68,10 +118,28 @@ export const DraggableSettings = () => {
     document.removeEventListener("mousemove", handleMouseMove)
     document.removeEventListener("mouseup", handleMouseUp)
   }
+  return (
+    <div className="flex justify-between rounded-t-sm bg-neutral-800 px-3 py-2 text-neutral-300">
+      <div
+        onMouseDown={handleMouseDown}
+        className="cursor-move select-none"
+        aria-label="hold and drag to move the settings"
+      >
+        <HandIcon />
+      </div>
+      <span className="font-bold uppercase">Settings</span>
+      <button onClick={props.toggleBodyCollapsed} className="cursor-pointer">
+        <span className="sr-only">collapse or open settings</span>
+        {props.isBodyCollapsed ? <ChevronsUpDownIcon /> : <ChevronsDownUpIcon />}
+      </button>
+    </div>
+  )
+})
+SettingHandle.displayName = "SettingHandle"
 
-  const [isCollapsed, setCollapsed] = useState(false)
-
-  const handleCollapse = () => setCollapsed((p) => !p)
+/* Body */
+const SettingBody = memo((props: { isBodyCollapsed: boolean }) => {
+  const { isBodyCollapsed } = props
 
   const {
     dragBehavior,
@@ -176,196 +244,171 @@ export const DraggableSettings = () => {
   const labelClassName = "flex gap-1 capitalize"
   const legendClassName = "grid grid-cols-2 items-center"
   const anchorClassName = "p-2 sm:p-3 bg-black rounded-full text-white"
-
   return (
     <div
-      className={"absolute z-50 flex max-h-[80svh] w-[40ch] flex-col overflow-hidden"}
-      style={{
-        transform: `translate(${position.x}px, ${position.y}px)`,
-        resize: isCollapsed ? "none" : "both",
-        height: isCollapsed ? "min-content" : "auto",
-      }}
+      className={twMerge(
+        clsx(isBodyCollapsed ? "h-0 py-0" : "h-max py-3"),
+        "grid max-h-full gap-6 overflow-y-scroll bg-neutral-300 px-3 accent-neutral-700"
+      )}
+      style={{ transition: "height 0.2s ease-in-out, padding 0.2s ease-in-out" }}
     >
-      {/* Handle and title */}
-      <div className="flex justify-between rounded-t-sm bg-neutral-800 px-3 py-2 text-neutral-300">
-        <div
-          onMouseDown={handleMouseDown}
-          className="cursor-move select-none"
-          aria-label="hold and drag to move the settings"
+      <div className="flex flex-wrap place-content-center gap-2">
+        <Button onClick={() => mazeProxy.logMaze()}>Log</Button>
+        <Button onClick={() => (modalProxy.open = true)}>Instructions</Button>
+        <Button onClick={() => mazeProxy.randomizeMaze()}>Randomize</Button>
+        <Button
+          onClick={() => {
+            mazeProxy.mazeProxy.nodes.forEach((node) => {
+              if (node?.state === "visited") node.state = "open"
+            })
+            mazeProxy.setIsMazeEditable(true)
+          }}
         >
-          <HandIcon />
-        </div>
-        <span className="font-bold uppercase">Settings</span>
-        <button onClick={handleCollapse} className="cursor-pointer">
-          <span className="sr-only">collapse or open settings</span>
-          {isCollapsed ? <ChevronsUpDownIcon /> : <ChevronsDownUpIcon />}
-        </button>
+          reset visited
+        </Button>
+        <Button
+          onClick={() => {
+            mazeProxy.resetAllNodesToOpen()
+            mazeProxy.setIsMazeEditable(true)
+          }}
+        >
+          reset
+        </Button>
+        <Button
+          onClick={() => {
+            if (selectedAlgorithm === "dfs") mazeProxy.runDfs()
+            if (selectedAlgorithm === "bfs") mazeProxy.runBfs()
+          }}
+        >
+          compute
+        </Button>
+        <Button
+          onClick={() => {
+            mazeProxy.resetAllNodesToOpen()
+            mazeProxy.setIsMazeEditable(true)
+            window.localStorage.clear()
+          }}
+        >
+          clear memory
+        </Button>
       </div>
 
-      {/* Collapsing Body */}
-      <div
-        className={twMerge(
-          clsx(isCollapsed ? "h-0 py-0" : "h-max py-3"),
-          "grid max-h-full gap-6 overflow-y-scroll rounded-bl-2xl bg-neutral-300 px-3 accent-neutral-700"
-        )}
-        style={{ transition: "height 0.2s ease-in-out, padding 0.2s ease-in-out" }}
-      >
-        <div className="flex flex-wrap place-content-center gap-2">
-          <Button onClick={() => mazeProxy.logMaze()}>Log</Button>
-          <Button onClick={() => (modalProxy.open = true)}>Instructions</Button>
-          <Button onClick={() => mazeProxy.randomizeMaze()}>Randomize</Button>
-          <Button
-            onClick={() => {
-              mazeProxy.mazeProxy.nodes.forEach((node) => {
-                if (node?.state === "visited") node.state = "open"
-              })
-              mazeProxy.setIsMazeEditable(true)
-            }}
-          >
-            reset visited
-          </Button>
-          <Button
-            onClick={() => {
-              mazeProxy.resetAllNodesToOpen()
-              mazeProxy.setIsMazeEditable(true)
-            }}
-          >
-            reset
-          </Button>
-          <Button
-            onClick={() => {
-              if (selectedAlgorithm === "dfs") mazeProxy.runDfs()
-              if (selectedAlgorithm === "bfs") mazeProxy.runBfs()
-            }}
-          >
-            compute
-          </Button>
-          <Button
-            onClick={() => {
-              mazeProxy.resetAllNodesToOpen()
-              mazeProxy.setIsMazeEditable(true)
-              window.localStorage.clear()
-            }}
-          >
-            clear memory
-          </Button>
-        </div>
+      <div className="grid gap-3">
+        <fieldset>
+          <legend className={legendClassName}>
+            Mouse Drag Behavior
+            <hr />
+          </legend>
 
-        <div className="grid gap-3">
-          <fieldset>
-            <legend className={legendClassName}>
-              Mouse Drag Behavior
-              <hr />
-            </legend>
+          <div className="grid">
+            {(["block node", "open node"] satisfies PossibleNodeInteractions[]).map((label) => (
+              <label key={label} className={labelClassName}>
+                <input
+                  type="radio"
+                  name={"drag action"}
+                  value={label}
+                  checked={label === dragBehavior}
+                  onChange={(e) => {
+                    const value = e.target.value
+                    possibleNodeInteractions.forEach((v) => {
+                      if (v === "block node" || v === "open node") {
+                        if (v === value) uiProxy.dragBehavior = value
+                      }
+                    })
+                  }}
+                />
+                <span>{label}</span>
+              </label>
+            ))}
+          </div>
+        </fieldset>
 
-            <div className="grid">
-              {(["block node", "open node"] satisfies PossibleNodeInteractions[]).map((label) => (
-                <label key={label} className={labelClassName}>
-                  <input
-                    type="radio"
-                    name={"drag action"}
-                    value={label}
-                    checked={label === dragBehavior}
-                    onChange={(e) => {
-                      const value = e.target.value
-                      possibleNodeInteractions.forEach((v) => {
-                        if (v === "block node" || v === "open node") {
-                          if (v === value) uiProxy.dragBehavior = value
-                        }
-                      })
-                    }}
-                  />
-                  <span>{label}</span>
-                </label>
-              ))}
-            </div>
-          </fieldset>
+        <fieldset>
+          <legend className={legendClassName}>
+            Left Click Behaviour
+            <hr />
+          </legend>
 
-          <fieldset>
-            <legend className={legendClassName}>
-              Left Click Behaviour
-              <hr />
-            </legend>
-
-            <div className="grid">
-              {possibleNodeInteractions.map((label) => (
-                <label key={label} className={labelClassName}>
-                  <input
-                    type="radio"
-                    name={"click action"}
-                    value={label}
-                    checked={label === clickBehavior}
-                    onChange={(e) => {
-                      const value = e.target.value
-                      possibleNodeInteractions.forEach((v) => {
-                        if (v === value) uiProxy.clickBehavior = value
-                      })
-                    }}
-                  />
-                  {label}
-                </label>
-              ))}
-            </div>
-          </fieldset>
-
-          <fieldset>
-            <legend className={legendClassName}>
-              Algorithm
-              <hr />
-            </legend>
-
-            <div className="grid">
-              {availableAlgorithms.map((label) => (
-                <label key={label} className={labelClassName}>
-                  <input
-                    type="radio"
-                    name={"selected algorithm"}
-                    value={label}
-                    checked={label === selectedAlgorithm}
-                    onChange={(e) => {
-                      const value = e.target.value
-                      availableAlgorithms.forEach((v) => {
-                        if (v === value) uiProxy.selectedAlgorithm = value
-                      })
-                    }}
-                  />
-                  {label}
-                </label>
-              ))}
-            </div>
-          </fieldset>
-
-          {inputs.map(({ label, inputProps }) => (
-            <div key={label} className="flex justify-between">
-              <label htmlFor={label} className="capitalize">
+          <div className="grid">
+            {possibleNodeInteractions.map((label) => (
+              <label key={label} className={labelClassName}>
+                <input
+                  type="radio"
+                  name={"click action"}
+                  value={label}
+                  checked={label === clickBehavior}
+                  onChange={(e) => {
+                    const value = e.target.value
+                    possibleNodeInteractions.forEach((v) => {
+                      if (v === value) uiProxy.clickBehavior = value
+                    })
+                  }}
+                />
                 {label}
               </label>
-              <input type="number" {...inputProps} className="py rounded-sm bg-neutral-200 px-2" />
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        </fieldset>
 
-        <div className="flex flex-wrap gap-1 sm:gap-3">
-          <a
-            target="_blank"
-            href="https://github.com/decipher-cs/pathfinder"
-            className={anchorClassName}
-            title="Source Code"
-          >
-            <GithubIcon />
-            <span className="sr-only">source code</span>
-          </a>
-          <a
-            target="_blank"
-            href="https://v2-superseded--path-visualize.netlify.app/"
-            className={anchorClassName}
-            title="Older Website"
-          >
-            <HistoryIcon />
-            <span className="sr-only">go to older website</span>
-          </a>
-        </div>
+        <fieldset>
+          <legend className={legendClassName}>
+            Algorithm
+            <hr />
+          </legend>
+
+          <div className="grid">
+            {availableAlgorithms.map((label) => (
+              <label key={label} className={labelClassName}>
+                <input
+                  type="radio"
+                  name={"selected algorithm"}
+                  value={label}
+                  checked={label === selectedAlgorithm}
+                  onChange={(e) => {
+                    const value = e.target.value
+                    availableAlgorithms.forEach((v) => {
+                      if (v === value) uiProxy.selectedAlgorithm = value
+                    })
+                  }}
+                />
+                {label}
+              </label>
+            ))}
+          </div>
+        </fieldset>
+
+        {inputs.map(({ label, inputProps }) => (
+          <div key={label} className="flex justify-between">
+            <label htmlFor={label} className="capitalize">
+              {label}
+            </label>
+            <input type="number" {...inputProps} className="py rounded-sm bg-neutral-200 px-2" />
+          </div>
+        ))}
+      </div>
+
+      <div className="flex flex-wrap gap-1 sm:gap-3">
+        <a
+          target="_blank"
+          href="https://github.com/decipher-cs/pathfinder"
+          className={anchorClassName}
+          title="Source Code"
+        >
+          <GithubIcon />
+          <span className="sr-only">source code</span>
+        </a>
+        <a
+          target="_blank"
+          href="https://v2-superseded--path-visualize.netlify.app/"
+          className={anchorClassName}
+          title="Older Website"
+        >
+          <HistoryIcon />
+          <span className="sr-only">go to older website</span>
+        </a>
       </div>
     </div>
   )
-}
+})
+
+SettingBody.displayName = "SettingBody"
